4848beeefa0b307581fb392bffe0ffce59c1d821
surajAli1998 <alisuraj649@gmail.com>
2024-04-12 23:47:18 +0530

    changes of 11/04/2024 added

---
+++ src/day_11April2024/Lab001.java
@@ -0,0 +1,24 @@
+package day_11April2024;
+
+//concept: comment lines
+public class Lab001 {
+    public static void main(String[] args) {
+
+        // this is single line comment
+
+        /*
+        this is multiline comments
+        A comment that can span multiple lines, starting with the marker
+        and ending with the marker. It is also ignored by the compiler and
+        used for adding longer explanations or temporarily disabling code.
+         */
+
+        /**
+         A comment that begins with /** and ends with.It is similar to a
+         * multiline comment, but it is specifically used for generating API
+         * documentation with tools like Javadoc.It can include tags like
+         * @param, @return, @throws,etc
+         */
+
+    }
+}
---
+++ src/day_11April2024/Lab002.java
@@ -0,0 +1,18 @@
+package day_11April2024;
+
+/**
+ * Author: Suraj
+ * Concept: System and println
+ */
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("Hello Suraj");
+        System.out.print("Welcome to ATB6X\n");
+        System.out.println("This is automation testing bootcamp\tfor all");
+
+        // \n will provide a new line explicitly, this is called escape character
+        // \t will provide a tab
+    }
+
+}
