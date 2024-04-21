11ac1534c4c9c6a5bfcf05273274b678bc01535d
surajAli1998 <alisuraj649@gmail.com>
2024-04-21 13:20:13 +0530

    main branch updated

--- src/day_18April2024/MergeConflict.java
+++ src/day_18April2024/MergeConflict.java
@@ -2,6 +2,8 @@ package day_18April2024;
 
 public class MergeConflict {
     public static void main(String[] args) {
+        int a = 2;
         System.out.println("Hello I am  Suraj");
+        a += 2;
     }
 }
