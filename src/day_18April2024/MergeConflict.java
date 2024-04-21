cb05c624d6e0f2faf1888ca8e6a01b2e24615e15
surajAli1998 <alisuraj649@gmail.com>
2024-04-21 13:21:56 +0530

    feature1 branch updated

--- src/day_18April2024/MergeConflict.java
+++ src/day_18April2024/MergeConflict.java
@@ -2,6 +2,8 @@ package day_18April2024;
 
 public class MergeConflict {
     public static void main(String[] args) {
+        float f = 34.5f;
         System.out.println("Hello I am  Suraj");
+        f += 2;
     }
 }
