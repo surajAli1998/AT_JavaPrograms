26062da97c52fb84c87c43ed19311ebe2cb93e69
surajAli1998 <alisuraj649@gmail.com>
2024-05-20 00:13:42 +0530

    chnaged

---
+++ src/day_18May2024/SuperWithConstructer.java
@@ -0,0 +1,5 @@
+package day_18May2024;
+
+public class SuperWithConstructer {
+
+}
---
+++ src/day_18May2024/ThisVsSuper.java
@@ -0,0 +1,34 @@
+package day_18May2024;
+
+public class ThisVsSuper {
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle();
+        Car c1 = new Car();
+//        System.out.println(c1.topSpeed);
+        c1.display();
+        c1.superDisplay();
+    }
+}
+
+class Vehicle{
+    int topSpeed = 100;
+    void display(){
+        System.out.println("This is Vehicle class");
+    }
+}
+
+class Car extends Vehicle{
+    int topSpeed = 120;
+    void display(){
+        System.out.println("This is Car class");
+        System.out.println("Max speed of this car is: "+this.topSpeed);
+//        super.display();
+//        System.out.println("Max speed of vehicle is: "+super.topSpeed);
+    }
+
+    void superDisplay(){
+        super.display();
+        System.out.println("Max speed of vehicle is: "+super.topSpeed);
+    }
+
+}
