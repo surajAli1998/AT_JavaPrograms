347befc081b5fd0df5c2906298d3ca6ea4ab4145
surajAli1998 <alisuraj649@gmail.com>
2024-06-07 12:48:36 +0530

    changes added

--- src/day_04May2024/string_questions/Anagram.java
+++ src/day_04May2024/string_questions/Anagram.java
@@ -1,6 +1,9 @@
 package day_04May2024.string_questions;
 
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  * Java Anagrams - silent -> listen , length1 = length2
@@ -13,22 +16,26 @@ public class Anagram {
         String str1 = sc.nextLine();
         System.out.println("Enter the second string: ");
         String str2 = sc.nextLine();
-        if(str1.length() == str2.length()){
-            boolean flag = true;
-            for(int i=0;i<str1.length();i++){
-                char ch = str1.charAt(i);
-                int index = str2.indexOf(ch);
-                int lastIndex = str2.lastIndexOf(ch);
-                if(index != -1){
-                    if(index != lastIndex){
-                        flag = false;
-                        break;
-                    }
-                }
-            }
-            if(flag) System.out.println("They are anagrams");
-            else System.out.println("They are not anagrams");
+//        char[] arr1 = str1.toCharArray();
+//        char[] arr2 = str2.toCharArray();
+//        Arrays.sort(arr1);
+//        Arrays.sort(arr2);
+//        str1 = Arrays.toString(arr1);
+//        str2 = Arrays.toString(arr2);
+//        if(str1.equalsIgnoreCase(str2)) System.out.println("They are Anagrams");
+//        else System.out.println("They are not Anagrams");
+
+        Map<Character,Integer> hmap1 = new TreeMap<>();
+        Map<Character,Integer> hmap2 = new TreeMap<>();
+        for(int i=0; i<str1.length(); i++){
+            Character ch = str1.charAt(i);
+            hmap1.put(ch,hmap1.get(ch)==null?1:hmap1.get(ch)+1);
         }
-        else System.out.println("They are not anagrams");
+        for(int i=0; i<str2.length(); i++){
+            Character ch = str2.charAt(i);
+            hmap2.put(ch,hmap2.get(ch)==null?1:hmap2.get(ch)+1);
+        }
+        if(hmap1.equals(hmap2)) System.out.println("Anagrams");
+        else System.out.println("Not Anagrams");
     }
 }
